171a99aed4383f44106ad71a3f75ae01292b282d
m.maesaka <r012mbmaesaka@gmail.com>
2024-05-18 09:55:45 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,59 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+import java.util.*;
+
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        list.add("apple");
+        list.add("orrange");
+        list.add("melon");
+
+//        要素数は3になる
+        System.out.println("要素数 = " + list.size());
+
+//        ループ処理
+        for(String fruits : list){
+            System.out.println(fruits);
+        }
+
+//        生徒数の文字列リスト
+        List<String> studentsList = List.of("田中","鈴木","牧野","たけのうち","いのうえ");
+        System.out.println(studentsList);
+
+//        リストに入っている文字数が3文字以上のものを表示する
+        for(String name : studentsList){
+            if(name.length() >= 3){
+                System.out.println("文字数が3文字以上のものを表示 : " + name);
+            }
+        }
+
+//        番号と生徒名の組み合わせのMapを作成
+//        Mapの要素は10個以上
+//        Mapの中の番号が偶数のものをMapから削除する
+//        上記処理を繰り返し行う処理を実装
+//        削除した状態のMapを出力して表示させる
+
+        Map<Integer , String> studentMap = new HashMap<>();
+        studentMap.put(1 ,"田中");
+        studentMap.put(2, "山田");
+        studentMap.put(3, "佐藤");
+        studentMap.put(4, "伊藤");
+        studentMap.put(5, "渡辺");
+
+        System.out.println("元のMap: " + studentMap);
+
+        Map<Integer , String> newStudentMap = new HashMap<>();
+        for(Map.Entry<Integer , String> entry : studentMap.entrySet()){
+            //        Mapの中の番号が偶数のものをMapから削除する
+            if(entry.getKey() %2 != 0){
+                newStudentMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        studentMap = newStudentMap;
+        System.out.println("削除した状態のMap: " + studentMap);
 
 //        図書管理システムの作成
 //        Listでもっている書籍情報を検索
